8eaa187eb9737a15422286e7efbe066f193c76b7
Jeevan640 <37647857+Jeevan640@users.noreply.github.com>
2018-04-07 12:44:51 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+# include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{	int n;
+	cout<<"ENTER THE NUMBER OF PROCESS\n";
+	cin>>n;
+	int temp=0;
+	double priority[n];
+	int process[n],burst[n],arrival[n],waiting[n];
+}
