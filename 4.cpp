6def81f83c781a7df6992f331b3b98d05a22f21f
Jeevan640 <37647857+Jeevan640@users.noreply.github.com>
2018-04-08 13:24:33 +0530

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,65 @@
+# include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{	int n;
+	cout<<"ENTER THE NUMBER OF PROCESS\n";
+	cin>>n;
+	int temp=0;
+	double priority[n];
+	int process[n],burst[n],arrival[n],waiting[n];
+
+	for (int i=0;i<n;i++)
+	{
+		process[i]=i+1;
+		priority[i]=0;
+	}
+	
+	cout<<"ENTER THE BURST TIMES FOR "<<n<<"PROCESSES";
+	
+	
+	for (int i=0;i<n;i++)
+	{
+		cin>>burst[i];
+	}
+	cout<<endl;
+	
+	
+	for(int i=0;i<n;i++)
+	{
+		int pos=i;
+		for(int j=i+1;j<n;j++)
+		{
+			if(burst[j]<burst[pos])
+				pos=j;
+		}
+		temp=burst[i];
+		burst[i]=burst[pos];
+		burst[pos]=temp;
+		
+		temp=process[i];
+		process[i]=process[pos];
+		process[pos]=temp;
+	 
+	 
+	}// cout<<"PROCESS "<<process[0]<<" HAS "<<waiting[0]<<"secs WAITING TIME"<<endl;
+	 waiting[0]=0,priority[0]=0;
+	 cout<<"PROCESS ID "<<process[0]<<" HAS SUCCESSFULLY EXECUTED"<<endl;
+	for(int i=1;i<n;i++)
+	{
+	
+	for(int k=i;k<n;k++)
+	 {
+	 	waiting[k]=0;
+	 	for(int j=0;j<k;j++)
+	 	{
+		 
+	 	waiting[k]+=burst[j];
+	 	
+	 }//cout<<"PROCESS "<<process[k]<<" HAS "<<waiting[k]<<"secs WAITING TIME"<<endl;
+	
+	 }
+	 cout<<endl;
+}}
